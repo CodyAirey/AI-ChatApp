55ab360c2c8f97840e596639d952e781aaaf07f1
Jake Norton <jake.norton88@gmail.com>
2023-08-30 09:48:12 +1200

    changed to version which boots properly initially to enable further work on webserver

--- build-aiserver-vm.sh
+++ build-aiserver-vm.sh
@@ -4,24 +4,7 @@ sudo apt-get update
 sudo apt-get install python3.10-venv python3.10-distutils -y
 curl https://bootstrap.pypa.io/get-pip.py | python3.10
 pip install --upgrade pip
-python3.10 -m pip install --ignore-installed flask huggingface transformers torch
+python3.10 -m pip install --ignore-installed flask huggingface transformers torch gunicorn
 sudo cp /vagrant/startup.sh /etc/init.d/startup.sh
 sudo chmod +x /etc/init.d/startup.sh
-sudo update-rc.d startup.sh defaults
-sudo /etc/init.d/startup.sh start
-
-# sudo apt-get install gcc -y
-# sudo apt-get install make -y
-# sudo apt-get install libssl-dev openssl -y
-# sudo apt-get install libffi-dev -y
-# sudo apt install zlib1g zlib1g-dev -y
-# sudo apt-get install -y tmux
-# wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
-# tar xzvf Python-3.10.0.tgz
-# cd Python-3.10.0
-# ./configure
-# make
-# sudo make install
-# cd ..
-# sudo rm -rf Python-3.10.0
-# rm Python-3.10.0.tgz 
+/etc/init.d/startup.sh
