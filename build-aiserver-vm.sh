ceea4c369980b841a35e89471a854e61d851b38c
Jake Norton <jake.norton88@gmail.com>
2023-08-30 10:35:49 +1200

    changed flask server to run on a service so that it will boot the server on reload

--- build-aiserver-vm.sh
+++ build-aiserver-vm.sh
@@ -4,7 +4,10 @@ sudo apt-get update
 sudo apt-get install python3.10-venv python3.10-distutils -y
 curl https://bootstrap.pypa.io/get-pip.py | python3.10
 pip install --upgrade pip
-python3.10 -m pip install --ignore-installed flask huggingface transformers torch gunicorn
-sudo cp /vagrant/startup.sh /etc/init.d/startup.sh
-sudo chmod +x /etc/init.d/startup.sh
-/etc/init.d/startup.sh
+python3.10 -m pip install --ignore-installed flask huggingface transformers torch 
+sudo cp /vagrant/server_chat.py /home/vagrant/
+sudo cp /vagrant/aiserver.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start aiserver
+sudo systemctl enable aiserver
+sudo systemctl status aiserver
