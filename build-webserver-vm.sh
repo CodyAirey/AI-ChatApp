22177e54a645cd68eb12235af21b93de0edb36e2
airco879 <airco879@student.otago.ac.nz>
2023-08-30 12:01:25 +1200

    bugfix; build.sh for webserv mkdir not persisting
    changed ~ to full directory route, now can actually see when vagrant ssh

--- build-webserver-vm.sh
+++ build-webserver-vm.sh
@@ -2,11 +2,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install --lts
 cd /vagrant
-cp -r nodejs-server ~
-cd ~/nodejs-server/
+cp -r nodejs-server /home/vagrant
+cd /home/vagrant/nodejs-server/
 ls
 npm install mysql2 express body-parser axios
 npm install -g pm2
 pm2 start server.js
 pm2 save
-# nohup node server.js &
+# pm2 logs server
