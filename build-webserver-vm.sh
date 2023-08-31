a5e840063b9cb144bc58304dbde3d4ca30ee282b
airco879 <airco879@student.otago.ac.nz>
2023-08-31 15:42:35 +1200

    removed build webserver artifact comments

--- build-webserver-vm.sh
+++ build-webserver-vm.sh
@@ -4,15 +4,9 @@ nvm install --lts
 cd /vagrant
 cp -r nodejs-server /home/vagrant
 cd /home/vagrant/nodejs-server/
-ls
 npm install mysql2 express body-parser axios
 sudo cp /vagrant/nodejs-server/node_chat_server.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start node_chat_server
 sudo systemctl enable node_chat_server
 sudo systemctl status node_chat_server
-
-# npm install -g pm2
-# pm2 start server.js
-# pm2 save
-# pm2 logs server
